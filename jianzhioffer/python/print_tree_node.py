4f381f4016f4a253b9082a8e4a2cda65cef98332
lryong <15816537946@163.com>
2019-02-21 00:31:42 +0800

    update

---
+++ jianzhioffer/python/print_tree_node.py
@@ -0,0 +1,31 @@
+# -*- coding:utf-8 -*-
+
+# 从上到下按层打印二叉树，同一层结点从左至右输出。每一层输出一行。
+
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+class Solution:
+    # 返回二维列表[[1,2],[4,5]]
+    def Print(self, pRoot):
+        # write code here
+        if not pRoot:
+            return []
+        ret = []
+        stack = [pRoot]
+        while stack:
+            res = []
+            nextStack = []
+            for i in stack:
+                res.append(i.val)
+                if i.left:
+                    nextStack.append(i.left)
+                if i.right:
+                    nextStack.append(i.right)
+            stack = nextStack
+            ret.append(res)
+        return ret
+
+
