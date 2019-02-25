b889aa11bf841c68b1a4e5ef6812ed8dc117cb1c
lryong <15816537946@163.com>
2019-02-25 23:47:11 +0800

    update

---
+++ jianzhioffer/python/print_TreeNode.py
@@ -0,0 +1,14 @@
+# -*- coding:utf-8 -*-
+# 请实现一个函数按照之字形打印二叉树，即第一行按照从左到右的顺序打印，第二层按照从右至左的顺序打印，第三行按照从左到右的顺序打印，其他行以此类推。
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+class Solution:
+    def Print(self, pRoot):
+        # write code here
+        if not pRoot:
+            return  []
+        stack = [pRoot]
+
